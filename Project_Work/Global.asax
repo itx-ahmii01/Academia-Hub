1d87fb7efda916e520b94a9139fa0aa70792765e
itx-ahmii01 <82256762+itx-ahmii01@users.noreply.github.com>
2021-11-23 18:33:23 +0500

    Files Added

---
+++ Project_Work/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="academia_hub.Global" Language="C#" %>
---
+++ Project_Work/Signup.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Configuration;
+
+namespace academia_hub
+{
+    public partial class Signup : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Signup_Click(object sender, EventArgs e)
+        {
+            String FullName = this.full.Text;
+        }
+    }
+}
